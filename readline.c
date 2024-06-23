ba2a572283dae96390fb972173d0f9ea34e1adb5
cpt <trombolinas@gmail.com>
2024-06-23 13:26:06 +0200

    separated var expansion

--- readline.c
+++ readline.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-// cc readline.c parser.c table.c -L libft -lreadline -lft
+// cc readline.c parser.c table.c hash.c load_envv.c pipes.c -L libft -lreadline -lft
 
 int	main()
 {
@@ -8,12 +8,12 @@ int	main()
 	char			*expin;
 	struct sigaction sa;
 	t_data			data;
-	t_test_env		testvars;
+	//t_test_env		testvars;
 
 	// interesting, arrays can be initialised like this
 	int	arr[3] = {-1};
 
-	data.vars = &testvars;
+	//data.vars = &testvars;
 	test_envvars(&data);
 
 //	return (0);
@@ -39,28 +39,13 @@ int	main()
  //        env++;
  //    }
 
-	// char exmp[] = "cat input.txt | tr ' ' '\n' | sort | uniq -c | sort -nr > word_count.txt";
-	// 
-	// char exmp2[] = "cat access.log | awk '{print $1}' | sort | uniq -c | sort -nr | head -n 10 > top_ips.txt";
-	// char exmp3[] = "grep -Eo '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}' emails.txt | sort | uniq > unique_emails.txt";
-	// char exmp4[] = "top -b -n 1 | grep \"Cpu(s)\" | awk '{print $2 + $4 \"% CPU\"}' > cpu_usage.txt";
-	// char exmp5[] = "cat /var/log/syslog /var/log/auth.log | grep -i \"error\" | sort | uniq > combined_errors.txt";
-	// tokenise(exmp, &data);
-	// tokenise(exmp2, &data);
-	// tokenise(exmp3, &data);
-	// tokenise(exmp4, &data);
-	// tokenise(exmp5, &data);
-	// print_tokens(ft_split(exmp, -1));
-	// print_tokens(ft_split(exmp2, -1));
-	// print_tokens(ft_split(exmp3, -1));
-	// print_tokens(ft_split(exmp4, -1));
-	// print_tokens(ft_split(exmp5, -1));
-
 	// while (1)
 	// {
 	// 	printf("zzz\n");
 	// 	sleep(1);
 	// }
+
+	//load_envv();
 
 	rl_initialize();
 	rl_readline_name = "minishell";
@@ -69,46 +54,44 @@ int	main()
 		input = readline(">>>");
 		if (input)
 		{
-			expin = expand_env(input, &data);
-			free(input);
-			tokenise(expin, &data);
+			add_history(input);
+			//expin = expand_env(input, &data);
+			//free(input);
+			tokenise(input, &data);
 			printf("########################\n");
-			print_tokens(ft_split(expin, -1));
+			data.tok = ft_split(input, -1);
+			print_tokens(data.tok);
 			// rl_replace_line("all chinese history is good and the government is perfect", 1);
 			// rl_redisplay();
 			// sleep(1);
 			// printf("Input added to history.\n");
-			add_history(expin);
-			free(expin);
+			//free(expin);
+			int i = 0;
+			while (data.tok[i])
+			{
+				if (is_var(&data, data.tok[i], '$'))
+				{
+					expand_envv(&data, data.tok[i]);
+					// free(data.tok[i]);
+					// data.tok[i] = data.expand;
+				}
+				i++;
+			}
+			printf("#########################\n");
+			print_tokens(data.tok);
 		}
 	append_history(3, "history");
 }
 
 int	test_envvars(t_data *data)
 {
-	// data->vars->key1 = malloc(11 * sizeof(char));
-	// wrt_to_str("one", data->vars->key1);
-	// data->vars->key2 = malloc(11 * sizeof(char));
-	// wrt_to_str("two", data->vars->key2);
-	// data->vars->var1 = malloc(33 * sizeof(char));
-	// wrt_to_str("unruffled", data->vars->var1);
-	// data->vars->var2 = malloc(6 * sizeof(char));
-	// wrt_to_str("sleep", data->vars->var2);
-	// 
-	// data->vars->head = create_node(data->vars->key1, data->vars->var1);
-	// data->vars->node1 = create_node(data->vars->key2, data->vars->var2);
-	// add_var(&data->vars->head, data->vars->node1);
-	//
-	// iter_table(data->vars->head, &print_node, data);
-	// //free_table(&data->vars->head);
-
 	init_arr(data, VAR_BUFF);
 	data->uev[poly_r_hash("one", VAR_BUFF)] = create_node("one", "unruffled");
 	data->uev[poly_r_hash("two", VAR_BUFF)] = create_node("two", "sleep");
 	data->uev[poly_r_hash("three", VAR_BUFF)] = create_node("three", "melatonine");
 	
-	printf("%s\n", data->uev[poly_r_hash("three", VAR_BUFF)]->key);
-	printf("%s\n", data->uev[poly_r_hash("three", VAR_BUFF)]->var);
+	printf("%s\n", data->uev[poly_r_hash("two", VAR_BUFF)]->key);
+	printf("%s\n", data->uev[poly_r_hash("two", VAR_BUFF)]->var);
 
 	return (0);
 }
