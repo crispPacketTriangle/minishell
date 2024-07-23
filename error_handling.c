99366601a4a55450a27ebfa5ad1d0b6491c115d2
Stephen Tobin <stephen.tobin@uni-potsdam.de>
2024-07-23 20:00:02 +0200

    Handles good and bad parses in script-style interface.
    We should consider how to distinguish script-based vs interactive shell.

--- error_handling.c
+++ error_handling.c
@@ -2,12 +2,9 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   error_handling.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+
-	+:+     */
-/*   By: linux <linux@student.42.fr>                +#+  +:+
-	+#+        */
-/*                                                +#+#+#+#+#+
-	+#+           */
+/*                                                    +:+ +:+	      +:+     */
+/*   By: linux <linux@student.42.fr>                +#+  +:+	   +#+        */
+/*                                                +#+#+#+#+#+	+#+           */
 /*   Created: 2024/06/23 15:29:16 by linux             #+#    #+#             */
 /*   Updated: 2024/07/12 16:23:50 by linux            ###   ########.fr       */
 /*                                                                            */
@@ -30,10 +27,14 @@ void	syntax_error(t_pdata *pdata, int n)
 
 	e1 = "unbash: syntax error near unexpected token `";
 	write(2, e1, ft_strlen(e1));
-	ft_putstr_fd(pdata->current, 2);
+	if (NULL != pdata->current)
+		ft_putstr_fd(pdata->current, 2);
+	else
+		ft_putstr_fd(pdata->last, 2);
 	e1 = "'\n";
 	write(2, e1, ft_strlen(e1));
 	free_data(pdata);
+	// printf("Would exit here with code %d\n", n);
 	exit(n);
 }
 
@@ -47,6 +48,7 @@ void	quote_error(t_pdata *pdata, int n)
 	e1 = "'\n";
 	write(2, e1, ft_strlen(e1));
 	free_data(pdata);
+	// printf("Would exit here with code %d\n", n);
 	exit(n);
 }
 
@@ -54,12 +56,13 @@ void	parens_error(t_pdata *pdata, int n)
 {
 	char *e1;
 
-	e1 = "unbash: syntax error--unmatched backet before or near token `";
+	e1 = "unbash: syntax error--unmatched bracket before or near token `";
 	write(2, e1, ft_strlen(e1));
 	ft_putstr_fd(pdata->current, 2);
 	e1 = "'\n";
 	write(2, e1, ft_strlen(e1));
 	free_data(pdata);
+	// printf("Would exit here with code %d\n", n);
 	exit(n);
 }
 
@@ -73,6 +76,7 @@ void	stack_error(t_pdata *pdata, int n)
 	e1 = "'\n";
 	write(2, e1, ft_strlen(e1));
 	free_data(pdata);
+	// printf("Would exit here with code %d\n", n);
 	exit(n);
 }
 
--- parser.c
+++ parser.c
@@ -2,12 +2,9 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+
-	+:+     */
-/*   By: linux <linux@student.42.fr>                +#+  +:+
-	+#+        */
-/*                                                +#+#+#+#+#+
-	+#+           */
+/*                                                    +:+ +:+         +:+     */
+/*   By: linux <linux@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+	+#+           */
 /*   Created: 2024/06/21 15:50:35 by linux             #+#    #+#             */
 /*   Updated: 2024/07/14 18:46:50 by linux            ###   ########.fr       */
 /*                                                                            */
@@ -51,9 +48,22 @@ void	check_parens(t_pdata *pdata)
 		parens_error(pdata, 9);
 }
 
+bool	handle_tail(t_pdata *pdata)
+{
+	if (!(pdata->current) && pdata->last && ft_strchr((const char *)"&|<>",
+			(int)*pdata->last))
+		syntax_error(pdata, 5);
+	if (pdata->last)
+	{
+		if (ft_strchr((const char *)"&|<>", (int)*pdata->last))
+			syntax_error(pdata, 5);
+	}
+	return (false);
+}
+
 void	handle_redirection(t_pdata *pdata)
 {
-	if (!pdata->current && pdata->last && ft_strchr((const char *)"|<>",
+	if (!(pdata->current) && pdata->last && ft_strchr((const char *)"|<>",
 			(int)*pdata->last))
 		syntax_error(pdata, 5);
 	if (pdata->last)
@@ -83,66 +93,99 @@ void	handle_pipes(t_pdata *pdata)
 	}
 }
 
+// void	handle_quotes(t_pdata *pdata)
+// {
+// 	int i;
+
+// 	i = 0;
+// 	while (pdata->current[i])
+// 	{
+// 		if ('\'' == pdata->current[i])
+// 		{
+// 			if (pdata->sqts == 0 && pdata->sqt_toggle <= 0)
+// 			{
+// 				pdata->sqts++;
+// 				pdata->sqt_toggle = 1;
+// 			}
+// 			else if (pdata->sqts && pdata->sqt_toggle == 0)
+// 			{
+// 				pdata->sqts++;
+// 				pdata->sqt_toggle = 1;
+// 			}
+// 			else if (pdata->sqts && pdata->sqt_toggle > 0)
+// 			{
+// 				pdata->sqts--;
+// 				pdata->sqt_toggle = 0;
+// 				if (!pdata->sqts && !pdata->dqts)
+// 					pdata->sqt_toggle = -1;
+// 			}
+// 			else
+// 				quote_error(pdata, 3);
+// 		}
+// 		else if ('\"' == pdata->current[i])
+// 		{
+// 			if (pdata->dqts == 0 && pdata->sqt_toggle != 0)
+// 			{
+// 				pdata->dqts++;
+// 				pdata->sqt_toggle = 0;
+// 			}
+// 			else if (pdata->dqts && pdata->sqt_toggle > 0)
+// 			{
+// 				pdata->dqts++;
+// 				pdata->sqt_toggle = 0;
+// 			}
+// 			else if (pdata->dqts && pdata->sqt_toggle == 0)
+// 			{
+// 				pdata->dqts--;
+// 				pdata->sqt_toggle = 1;
+// 				if (!pdata->sqts && !pdata->dqts)
+// 					pdata->sqt_toggle = -1;
+// 			}
+// 			else
+// 				quote_error(pdata, 3);
+// 			// if (pdata->dqts && !pdata->sqt_toggle)
+// 			// 	pdata->dqts--;
+// 			// else
+// 			// 	pdata->dqts++;
+// 			// pdata->sqt_toggle = !pdata->sqt_toggle;
+// 		}
+// 		i++;
+// 	}
+// 	if (pdata->sqts || pdata->dqts)
+// 		quote_error(pdata, 3);
+// }
+
 void	handle_quotes(t_pdata *pdata)
 {
 	int i;
 
 	i = 0;
-	while (pdata->current[i])
+	if (pdata->current && pdata->current[i] && '\'' == pdata->current[i])
 	{
-		if ('\'' == pdata->current[i])
-		{
-			if (pdata->sqts == 0 && pdata->sqt_toggle <= 0)
-			{
-				pdata->sqts++;
-				pdata->sqt_toggle = 1;
-			}
-			else if (pdata->sqts && pdata->sqt_toggle == 0)
-			{
-				pdata->sqts++;
-				pdata->sqt_toggle = 1;
-			}
-			else if (pdata->sqts && pdata->sqt_toggle > 0)
-			{
-				pdata->sqts--;
-				pdata->sqt_toggle = 0;
-				if (!pdata->sqts && !pdata->dqts)
-					pdata->sqt_toggle = -1;
-			}
-			else
-				quote_error(pdata, 3);
-		}
-		else if ('\"' == pdata->current[i])
-		{
-			if (pdata->dqts == 0 && pdata->sqt_toggle != 0)
-			{
-				pdata->dqts++;
-				pdata->sqt_toggle = 0;
-			}
-			else if (pdata->dqts && pdata->sqt_toggle > 0)
-			{
-				pdata->dqts++;
-				pdata->sqt_toggle = 0;
-			}
-			else if (pdata->dqts && pdata->sqt_toggle == 0)
-			{
-				pdata->dqts--;
-				pdata->sqt_toggle = 1;
-				if (!pdata->sqts && !pdata->dqts)
-					pdata->sqt_toggle = -1;
-			}
-			else
-				quote_error(pdata, 3);
-			// if (pdata->dqts && !pdata->sqt_toggle)
-			// 	pdata->dqts--;
-			// else
-			// 	pdata->dqts++;
-			// pdata->sqt_toggle = !pdata->sqt_toggle;
-		}
 		i++;
+		pdata->sqts++;
+		pdata->sqt_toggle = 1;
+		while ('\0' != pdata->current[i] && '\'' != pdata->current[i])
+			i++;
+		if ('\'' == pdata->current[i])
+		pdata->sqts--;
+		if (pdata->sqts)
+			quote_error(pdata, 3);
+		pdata->sqt_toggle = -1;
 	}
-	if (pdata->sqts || pdata->dqts)
-		quote_error(pdata, 3);
+	else if (pdata->current && pdata->current[i] && '\"' == pdata->current[i])
+	{
+		i++;
+		pdata->dqts++;
+		pdata->sqt_toggle = 0;
+		while ('\0' != pdata->current[i] && '\"' != pdata->current[i])
+			i++;
+		if ('\"' == pdata->current[i])
+			pdata->dqts--;
+		if (pdata->dqts)
+			quote_error(pdata, 3);
+		pdata->sqt_toggle = -1;
+	}
 }
 
 void	update_token(t_pdata *pdata, char *token)
@@ -158,7 +201,7 @@ void	update_token(t_pdata *pdata, char *token)
 		free(pdata->current);
 		pdata->current = NULL;
 	}
-	if (token && ft_isprint((int)*token))
+	if (token) // && ft_isprint((int)*token))
 		pdata->current = ft_strdup((const char *)token);
 }
 
@@ -193,21 +236,35 @@ int	parse_tokens(char **tokens)
 	initialize_pdata(&pdata);
 	update_token(&pdata, tokens[i]);
 	check_start(&pdata);
-	while (tokens[i] && ft_isprint((int)(*tokens[i])))
+	while (tokens[i])
 	{
-		update_token(&pdata, tokens[i]);
-		// if (pdata.current)
-		// {
-		if ('\"' == *pdata.current || '\'' == *pdata.current)
-			handle_quotes(&pdata);
-		else if ('|' == *pdata.current)
-			handle_pipes(&pdata);
-		else if ('<' == *pdata.current || '>' == *pdata.current)
-			handle_redirection(&pdata);
+		if (pdata.current == NULL)
+		{
+			if (!handle_tail(&pdata))
+				break ;
+		}
+		else if (!*pdata.current)
+		{
+			if (!handle_tail(&pdata))
+				break ;
+		}
 		else
-			check_parens(&pdata);
-		// }
-		i++;
+		{
+			if ('\"' == *pdata.current || '\'' == *pdata.current)
+				handle_quotes(&pdata);
+			else if ('|' == *pdata.current)
+				handle_pipes(&pdata);
+			else if ('<' == *pdata.current || '>' == *pdata.current)
+				handle_redirection(&pdata);
+			else
+				check_parens(&pdata);
+			if (tokens[i + 1])
+				update_token(&pdata, tokens[++i]);
+			else
+				update_token(&pdata, NULL);
+		}
+		if (!pdata.current && !pdata.last)
+			return (0);
 	}
 	return (0);
 }
@@ -217,144 +274,65 @@ int	parse_tokens(char **tokens)
 
 int	main(void)
 {
-	// int i;
 	int res;
 	char **tokens;
+
 
 	char *good_commands[][10] = {{"echo", "\"Hello, World!\"", NULL},
 									{"ls", "-l", "/home/user", NULL},
 									{"ls", ">", "outfile.txt", NULL},
 									{"cat", "<", "infile.txt", NULL},
-									{"grep", "\"search_term\"", "<",
-										"infile.txt", ">", "outfile.txt",
-										NULL},
-									{"ls", "|", "grep", "\"search_term\"",
-										NULL},
+									{"ls", ">>", "outfile.txt", NULL},
+									{"cat", "<<", "infile.txt", NULL},
+									{"grep", "\"search_term\"", "<", "infile.txt", ">", "outfile.txt", NULL},
+									{"ls", "|", "grep", "\"search_term\"", NULL},
 									{"cat", "file.txt", "|", "wc", "-l", NULL},
-									{"mkdir", "new_dir", "&&", "cd", "new_dir",
-										NULL},
-									{"touch", "file1", "&&", "echo",
-										"\"content\"", ">", "file1", NULL},
-									{"false", "||", "echo",
-										"\"This runs because the previous command failed\"",
-										NULL},
-									// // {"true", "||", "echo",
-									// 	"\"This won't run because the previous command succeeded\"",
-									// // 	NULL},
+									{"mkdir", "new_dir", "&&", "cd", "new_dir", NULL},
+									{"touch", "file1", "&&", "echo", "\"content\"", ">", "file1", NULL},
+									{"false", "||", "echo","\"This runs because the previous command failed\"", NULL},
+									{"true", "||", "echo", "\"This won't run because the previous command succeeded\"", NULL},
 									{"(cd /tmp && ls)", NULL},
 									{"{echo \"Hello\" echo \"World\";}", NULL},
-									{"echo", "\"This is a string with spaces\"",
-										NULL},
-									{"echo",
-										"'This is another string with spaces'",
-										NULL},
-									{"echo",
-										"\"Mixing 'single' and \\\"double\\\" quotes\"",
-										NULL},
-									{"echo", "\"Current date: $(date)\"",
-										NULL},
+									{"echo", "\"This is a string with spaces\"", NULL},
+									{"echo", "'This is another string with spaces'", NULL},
+									{"echo", "\"Mixing 'single' and \\\"double\\\" quotes\"", NULL},
+									{"echo", "\"Current date: $(date)\"", NULL},
 									{"echo", "\"Files: $(ls)\"", NULL},
-									{"echo", "\"Hello, World!\"", "&&", "ls",
-										"-l", "|", "grep", "\"txt\"", NULL},
-									{"(echo \"Start\" && ls)", "||", "echo",
-										"\"Failed\"", NULL},
-									{"{echo \"Block 1\"; echo \"Block 2\";}",
-										">", "blocks.txt", NULL},
-									{"echo", "\"$(date): File list\"", "&&",
-										"ls", ">", "filelist.txt", NULL},
+									{"echo", "\"Hello, World!\"", "&&", "ls", "-l", "|", "grep", "\"txt\"", NULL},
+									{"(echo \"Start\" && ls)", "||", "echo", "\"Failed\"", NULL},
+									{"{echo \"Block 1\"; echo \"Block 2\";}", ">", "blocks.txt", NULL},
+									{"echo", "\"$(date): File list\"", "&&", "ls", ">", "filelist.txt", NULL},
 									{NULL}};
+
+
 	// Bad commands
-	// 	char *bad_commands[][10] = {/*// {"echo",
-	// "\"This is an unmatched quote",
-	// 																				// NULL},
-	// 																				// {"echo",
-	// 																					"'This is another unmatched quote",
-	// 																				// 	NULL},
-	// 																				// {NULL}};
-	// 																				// {"echo",
-	// 																					"(unmatched",
-	// 																					"parentheses",
-	// 																					NULL},
-	// 																				// {"echo",
-	// 																					"unmatched",
-	// 																					"parentheses)",
-	// 																					NULL},
-	// 																				// {"echo",
-	// 																					"{unmatched",
-	// 																					"braces",
-	// 																						NULL},
-	// 																				// {"echo",
-	// 																					"unmatched",
-	// 																					"braces}",
-	// 																						NULL},
-	// 																				// {"echo",
-	// 																					"[unmatched",
-	// 																					"brackets",
-	// 																						NULL},
-	// 																				// {"echo",
-	// 																					"unmatched",
-	// 																					"brackets]",
-	// 																						NULL},
-	// 																				// {"|",
-	// 																				// 	"echo",
-	// 																				// 	"\"Pipe at the beginning\"",
-	// 																				// 	NULL},
-	// 																				// {"echo",
-	// 																					"\"Pipe at the end\"",
-	// 																					"|",
-	// 																						NULL},
-	// 														{"echo",
-	// 															"\"Double pipe\"",
-	// 															"||",
-	// 															"echo",
-	// 															"\"another command\"",
-	// 																NULL},
+	char *bad_commands[][10] = {
+/*								{"echo", "\"This is an unmatched quote", NULL},
+								{"echo", "'This is another unmatched quote", NULL},
+								{"echo", "(unmatched", "parentheses", NULL},
+								{"echo", "unmatched", "parentheses)", NULL},
+								{"echo", "{unmatched", "braces", NULL},
+								{"echo", "unmatched", "braces}", NULL},
+								{"echo", "[unmatched", "brackets", NULL},
+								{"echo", "unmatched", "brackets]", NULL},
+								{"|", "echo", "\"Pipe at the beginning\"", NULL},
+								{"echo", "\"Pipe at the end\"", "|", NULL},
+//								{">", "outfile.txt", "echo", "\"Redirection at the beginning\"", NULL},
+								{"echo", "\"Redirection at the end\"", ">", NULL},						// NOT AN ERROR CASE!
+								{"echo", "\"Double redirection\"", ">>", ">>", "outfile.txt", NULL},
+								{"&&", "echo", "\"Logical AND at the beginning\"", NULL},
+								{"echo", "\"Logical AND at the end\"", "&&", NULL},
+								{"||", "echo", "\"Logical OR at the beginning\"", NULL},
+								{"echo", "\"Logical OR at the end\"", "||", NULL},
+								{"echo", "*", "|", NULL},
+//								{"echo", "|", "*", NULL}, 												// NOT AN ERROR CASE!
+								{"echo", "\"Mismatched quotes'", "&&", "ls", NULL},
+								{"(", "echo", "\"Missing closing parenthesis\"", "||", "ls", NULL},
+								{"echo", "\"Redirection in the middle\"", ">", "|", "grep", "\"error\"", NULL},
+								{"echo", "\"Logical AND at end\"", "&&", NULL},
+//								{"echo", "\"Unmatched Q im cmd subst $(date && echo 'Done)\"", NULL},	// NOT AN ERROR CASE!
+*/								{NULL}};
 
-	// 													{">", "outfile.txt",
-	// 														"echo",
-	// 														"\"Redirection at the beginning\"",
-	// 															NULL},
-
-	// 												{"echo",
-	// 													"\"Redirection at the end\"",
-	// 													">",
-	// 													NULL},
-
-	// 												{"echo",
-	// 													"\"Double redirection\"",
-	// 													">>",
-	// 													">>",
-	// 													"outfile.txt", NULL},
-
-	// 												{"&&", "echo",
-	// 													"\"Logical AND at the beginning\"",
-	// 														NULL},
-
-	//  {"echo", "\"Logical AND at the end\"", "&&", NULL},
-	// 						{"||", "echo", "\"Logical OR at the beginning\"",
-	// 							NULL},
-	//  {"echo", "\"Logical OR at the end\"", "||", NULL},
-
-	// 												{"echo", "*", "|", NULL},
-
-	// 											{"echo", "|", "*", NULL},
-
-	// 						{"echo", "\"Mismatched quotes'", "&&", "ls", NULL},
-	//  {"(", "echo", "\"Missing closing parenthesis\"", "||", "ls", NULL},
-
-	// 				{"echo", "\"Redirection in the middle\"", ">", "|", "grep",
-	// 					"\"error\"", NULL},
-	// 					{"echo", "\"Logical AND at end\"", "&&", NULL},
-	// 	{
-	// 		NULL
-	// 	}
-	// };
-	// 			*/
-	// 								{"echo",
-	// 									"\"Unmatched Q im cmd subst $(date\
-// 									&& echo 'Done)\"",
-	// 									NULL},
-	// 								{NULL}};
 
 	printf("Parsing good commands:\n");
 	for (int i = 0; i < (int)(sizeof(good_commands)
@@ -365,15 +343,16 @@ int	main(void)
 		if (res == 0)
 			printf("SUCCESSFUL PARSE\n");
 	}
-	// printf("\nParsing bad commands:\n");
-	// for (int i = 0; i < (int)(sizeof(bad_commands)
-	// 		/ sizeof(bad_commands[0])); i++)
-	// {
-	// 	tokens = bad_commands[i];
-	// 	parse_tokens(tokens);
-	// }
-	//		// tokens = &token;
-	//		// parse_tokens(tokens);
-	//		// printf("SUCCESSFUL PARSE\n");
+
+//	(void)good_commands;
+//	(void)res;
+
+	printf("\nParsing bad commands:\n");
+	for (int i = 0; i < (int)(sizeof(bad_commands)
+			/ sizeof(bad_commands[0])); i++)
+	{
+		tokens = bad_commands[i];
+		parse_tokens(tokens);
+	}
 	return (0);
 }
